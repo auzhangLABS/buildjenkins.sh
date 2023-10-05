e57e4df4f274b0add2fe042e9199eedacd395e95
Aubrey Zhang <138344000+auzhangLABS@users.noreply.github.com>
2023-10-05 18:48:10 -0400

    Update buildjenkins.sh

--- buildjenkins.sh
+++ buildjenkins.sh
@@ -4,6 +4,6 @@ curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \/usr/sh
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \https://pkg.jenkins.io/debian binary/ | sudo tee \/etc/apt/sources.list.d/jenkins.list > /dev/null
 
 sudo apt-get update
-sudo apt-get -y install fontconfig openjdk-17-jre
+sudo apt install -y fontconfig openjdk-17-jre
 
-sudo apt-get -y install jenkins
+sudo apt -y install jenkins
